4a0c51d622dfb12e07a316dcc38e2800b6eb72e1
DT-Kim <398546027@qq.com>
2018-08-30 15:49:42 +0800

    base

---
+++ php/conn.php
@@ -0,0 +1,13 @@
+<?php
+    $servername = "127.0.0.1:3306";
+    $username = "root";
+    $password = "123456";
+    $dbname ="tongbei";
+    $conn = new mysqli($servername, $username, $password, $dbname); 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }else{
+//      echo "Connected successfully 成功！";
+    }
+    
+?>
